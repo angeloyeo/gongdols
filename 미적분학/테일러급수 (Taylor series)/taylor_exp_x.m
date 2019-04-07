cb664190c2449c208c828afd0ade9db3b601b741
Donghoon Yeo <angeloyeo@gmail.com>
2019-04-07 19:21:04 +0900

    Add files via upload

--- 미적분학/테일러급수 (Taylor series)/taylor_exp_x.m
+++ 미적분학/테일러급수 (Taylor series)/taylor_exp_x.m
@@ -1,25 +1,27 @@
 clear; close all; clc;
 
-x = linspace(-3,3,20);
+XLIMs = [-2*pi, 2*pi];
+YLIMs = [-1 4];
+x = linspace(XLIMs(1), XLIMs(2) ,100);
 y = exp(x);
 
 plot(x,y,'linewidth',2)
 grid on;
 hold on;
-line([-3 3],[0 0],'color','k')
-line([0 0],[-1 8],'color','k')
-ylim([-1 8])
+line([0 0],[YLIMs(1) YLIMs(2)],'color','k')
+line([XLIMs(1) XLIMs(2)],[0 0],'color','k')
+xlim(XLIMs)
+ylim(YLIMs)
 
-app_point = 0;
+ExpansionPoint = 1 ;
 
-taylor_e = 0;
-for n= 0:10
-    t_e_nth_term = (x-app_point).^n/factorial(n);
+for n= 1:10
     
-    taylor_e = taylor_e + t_e_nth_term;
-    h = plot(x,taylor_e,'r','linewidth',2);
-    xlabel('x'); ylabel('y');
-    
+    syms x
+    f = exp(x);
+    T = taylor(f, 'ExpansionPoint', ExpansionPoint, 'Order', n);
+    h = fplot(T,XLIMs,'linewidth',2,'color','r');
+
     title('e^x and its Taylor series');
     tp1 = 0.8;
     tp2 = 0.5;
@@ -33,10 +35,9 @@ for n= 0:10
         t = text(tp1,tp2,['added upto ',num2str(n),'th term']);
     end
     t.FontSize=12;
-    pause(1);
+    pause;
     if n<10
         delete(h);
         delete(t);
     end
-    
 end
--- 미적분학/테일러급수 (Taylor series)/taylor_sin_x.m
+++ 미적분학/테일러급수 (Taylor series)/taylor_sin_x.m
@@ -1,31 +1,30 @@
 clear; close all; clc;
 
-XLIMs = [-3*pi 3*pi];
-YLIMs = [-1.5 1.5];
-x = linspace(XLIMs(1),XLIMs(2),100);
+XLIMs = [-2*pi, 2*pi];
+YLIMs = [-2 2];
+x = linspace(XLIMs(1), XLIMs(2) ,100);
 y = sin(x);
 
 plot(x,y,'linewidth',2)
 grid on;
 hold on;
-line(XLIMs,[0 0],'color','k')
-line([0 0],YLIMs,'color','k')
+line([0 0],[YLIMs(1) YLIMs(2)],'color','k')
+line([XLIMs(1) XLIMs(2)],[0 0],'color','k')
 xlim(XLIMs)
 ylim(YLIMs)
 
-app_point = 0;
+ExpansionPoint = 1;
 
-taylor_sin = 0;
-for n= 0:10
-    t_sin_nth_term = (-1).^n/factorial(2*n+1)*x.^(2*n+1);
+for n= 1:10
     
-    taylor_sin = taylor_sin + t_sin_nth_term;
-    h = plot(x,taylor_sin,'r','linewidth',2);
-    xlabel('x'); ylabel('y');
-    
-    title('sin(x) and its Taylor series');
-    tp1 = 2.5;
-    tp2 = -1.25;
+    syms x
+    f = sin(x);
+    T = taylor(f, 'ExpansionPoint', ExpansionPoint, 'Order', n);
+    h = fplot(T,XLIMs,'linewidth',2,'color','r');
+
+    title('e^x and its Taylor series');
+    tp1 = 0.8;
+    tp2 = -1.5;
     if n==1
         t = text(tp1,tp2,['added upto 1st term']);
     elseif n==2
@@ -36,10 +35,9 @@ for n= 0:10
         t = text(tp1,tp2,['added upto ',num2str(n),'th term']);
     end
     t.FontSize=12;
-    pause(1);
+    pause;
     if n<10
         delete(h);
         delete(t);
     end
-    
 end
