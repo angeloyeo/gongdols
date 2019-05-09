384ff45dd6a45412e3e712168a2a120444210964
angeloyeo <angeloyeo@gmail.com>
2019-05-09 18:50:17 +0900

    로피탈 정리의 기하학적 의미 / Fig1 그리기

--- 미적분학/로피탈정리의_기하학적의미/lhopital_rule.m
+++ 미적분학/로피탈정리의_기하학적의미/lhopital_rule.m
@@ -1,8 +1,13 @@
 clear; close all; clc;
 
-x = linspace(-100,100,500);
+%% Fig 1
+x1 = -500;
+x2 = 500;
+x = linspace(x1,x2,500000);
 % y = 1/3*x + 20*exp(-x.^2/100) -50*exp(-(x-40).^2/100)-20*exp(-(x+40).^2/100) + sin(x).*exp(-x.^2);
-y = 1/3*x + 20*sin(x/10).*exp(-x.^2/50000);
+y = 1/3*x + 20*sin(x/10).*exp(-x.^2/50000) + sin(x*10);
 
-plot(x,y)
+plotXY(x1,x2,x1,x2, true)
 grid on;
+hold on;
+plot(x,y)
