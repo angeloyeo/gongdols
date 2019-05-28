f7e9143c08e67f1b893e8f4e1f7dedadbe5859a8
angeloyeo <angeloyeo@gmail.com>
2019-05-28 13:08:01 +0900

    MATLAB animation 4강 / Grid 변하는 선형변환

--- MATLAB강의/animation만들기/animation_04_linear_transfomation_grid.m
+++ MATLAB강의/animation만들기/animation_04_linear_transfomation_grid.m
@@ -10,37 +10,45 @@ vert_d = [YLIMs(1):YLIMs(2); ones(1,diff(YLIMs)+1)*YLIMs(1)];
 
 hori_r = [ones(1,diff(YLIMs)+1)*XLIMs(2); YLIMs(1):YLIMs(2)];
 hori_l = [ones(1,diff(YLIMs)+1)*XLIMs(1); YLIMs(1):YLIMs(2)];
-figure;
-for i=1:size(vert_u,2)
-    line([vert_u(1,i) vert_d(1,i)],[vert_u(2,i) vert_d(2,i)],'color','k');
-    hold on;
-    line([hori_l(1,i) hori_r(1,i)],[hori_l(2,i) hori_r(2,i)],'color','k');
-end
+% figure;
+% for i=1:size(vert_u,2)
+%     line([vert_u(1,i) vert_d(1,i)],[vert_u(2,i) vert_d(2,i)],'color','k');
+%     hold on;
+%     line([hori_l(1,i) hori_r(1,i)],[hori_l(2,i) hori_r(2,i)],'color','k');
+% end
 
 xlim(XLIMs)
 ylim(YLIMs)
 
-figure;
 Avert_u = A*vert_u;
 Avert_d = A*vert_d;
 Ahori_r = A*hori_r;
 Ahori_l = A*hori_l;
-figure;
-for i=1:size(vert_u,2)
-    line([Avert_u(1,i) Avert_d(1,i)],[Avert_u(2,i) Avert_d(2,i)],'color','k');
-    hold on;
-    line([Ahori_l(1,i) Ahori_r(1,i)],[Ahori_l(2,i) Ahori_r(2,i)],'color','k');
-end
-xlim(XLIMs)
-ylim(YLIMs)
+% figure;
+% for i=1:size(vert_u,2)
+%     line([Avert_u(1,i) Avert_d(1,i)],[Avert_u(2,i) Avert_d(2,i)],'color','k');
+%     hold on;
+%     line([Ahori_l(1,i) Ahori_r(1,i)],[Ahori_l(2,i) Ahori_r(2,i)],'color','k');
+% end
+% xlim(XLIMs)
+% ylim(YLIMs)
 
 %% animation으로 만들기
-A = [2,1;1,2];
+% A = [2,1;1,2];
 % theta = pi/3;
 % A = [cos(theta) -sin(theta);sin(theta) cos(theta)];
 
+% A = [0,1;1,0];
+% 
+vec = [1,2]';
+A = vec*vec';
+
+v = VideoWriter('ani4_proj.mp4','MPEG-4');
+open(v);
 n_steps = 100;
 figure;
+set(gcf,'color','w');
+
 step_mtx = (A-eye(2))/n_steps;
 for i_steps = 0:n_steps
 %     figure;
@@ -61,12 +69,14 @@ for i_steps = 0:n_steps
     end
     xlim(XLIMs);
     ylim(YLIMs);
+%     
+%     if i_steps ==0
+%         pause;
+%     end
     
-    if i_steps ==0
-        pause;
-    end
-    
-    pause(0.01);
+%     pause(0.01);
+    F(i_steps+1) = getframe(gcf);
+
     if i_steps<n_steps
         cla;
     end
@@ -74,3 +84,5 @@ for i_steps = 0:n_steps
 end
 
     
+writeVideo(v,F);
+close(v)
