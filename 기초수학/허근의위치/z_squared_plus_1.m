21199bda01850e02fd75182d60e54c0681af8132
angeloyeo <angeloyeo@gmail.com>
2019-07-01 15:26:08 +0900

    Update z_squared_plus_1.m

--- 기초수학/허근의위치/z_squared_plus_1.m
+++ 기초수학/허근의위치/z_squared_plus_1.m
@@ -33,29 +33,29 @@ title('complex plane')
 h1 = annotation('arrow');  % store the arrow information in ha
 h1.Parent = gca;           % associate the arrow the the current axes
 h1.X = [0 0];          % the location in data units
-h1.Y = [0 1.5];   
+h1.Y = [0 1.5];
 
 h2 = annotation('arrow');  % store the arrow information in ha
 h2.Parent = gca;           % associate the arrow the the current axes
 h2.X = [0 0];          % the location in data units
-h2.Y = [0 -1.5];   
+h2.Y = [0 -1.5];
 
 
 h3 = annotation('arrow');  % store the arrow information in ha
 h3.Parent = gca;           % associate the arrow the the current axes
 h3.X = [0 1.5];          % the location in data units
-h3.Y = [0 0];   
+h3.Y = [0 0];
 
 h4 = annotation('arrow');  % store the arrow information in ha
 h4.Parent = gca;           % associate the arrow the the current axes
 h4.X = [0 -1.5];          % the location in data units
-h4.Y = [0 0];   
+h4.Y = [0 0];
 
 %% complex space
 
 real_axis=linspace(-2,2,30);
 imag_axis=linspace(-2,2,30);
- 
+
 fz= zeros(length(real_axis),length(imag_axis));
 for iter_r=1:length(real_axis)
     for iter_i=1:length(imag_axis)
@@ -68,8 +68,8 @@ surf(real_axis,imag_axis,abs(fz'),angle(fz'));
 xlabel('Real(f(z))');
 ylabel('Imag(f(z))');
 zlabel('Mag(f(z))');
-h = colorbar;
-ylabel(h, 'Phase(f(z))')
+% h = colorbar;
+% ylabel(h, 'Phase(f(z))')
 hold on
- 
+
 plot3(real_axis,zeros(size(real_axis)),[real_axis.^2+1],'r','linewidth',5)
