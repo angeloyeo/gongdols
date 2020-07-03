61df2db69a22095221a0ce2f796863db89bc5ded
angeloyeo <angeloyeo@gmail.com>
2020-07-03 18:02:43 +0900

    Update Frangi_Filter.py

--- 미적분학/헤시안 행렬의 의미/Vessel_Detction_Python/Frangi_Filter.py
+++ 미적분학/헤시안 행렬의 의미/Vessel_Detction_Python/Frangi_Filter.py
@@ -5,26 +5,53 @@ Created on Fri Jul  3 11:52:09 2020
 @author: biosensor1
 """
 
-from skimage.data import camera
-from skimage.filters import frangi, hessian
-
+# from skimage.data import camera
+from skimage.filters import frangi
+from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
 
-image = camera()
+# image = camera()
+image = np.asarray(Image.open('vessel.png'))
+plt.close('all')
 
+fig, ax = plt.subplots(ncols=2)
+
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Original image')
+
+ax[1].imshow(
+    frangi(image),
+    cmap=plt.cm.gray, vmin = 0, vmax = 0.00001)
+ax[1].set_title('Frangi filter result')
+
+for a in ax:
+    a.axis('off')
+plt.tight_layout()
+plt.show()
+
+#%% Hessian의 Eigen value만 plot
+
+from skimage.feature import hessian_matrix, hessian_matrix_eigvals
+
+hxx, hxy, hyy = hessian_matrix(image, sigma = 3)
+
+i1, i2 = hessian_matrix_eigvals([hxx, hxy, hyy])
+
+plt.close('all')
 fig, ax = plt.subplots(ncols=3)
 
 ax[0].imshow(image, cmap=plt.cm.gray)
 ax[0].set_title('Original image')
 
-ax[1].imshow(frangi(image), cmap=plt.cm.gray)
-ax[1].set_title('Frangi filter result')
+ax[1].imshow(i1, cmap = plt.cm.gray)
+ax[1].set_title('i1')
 
-ax[2].imshow(hessian(image), cmap=plt.cm.gray)
-ax[2].set_title('Hybrid Hessian filter result')
+ax[2].imshow(i2, cmap = plt.cm.gray)
+ax[2].set_title('i2')
+
 
 for a in ax:
     a.axis('off')
-
 plt.tight_layout()
 plt.show()
