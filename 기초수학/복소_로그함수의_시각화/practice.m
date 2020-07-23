808841e7fd06145b0748fcd658a40bf67b537adf
angeloyeo <angeloyeo@gmail.com>
2020-07-23 11:31:01 +0900

    복소 로그함수 그림 업로드

--- 기초수학/복소_로그함수의_시각화/practice.m
+++ 기초수학/복소_로그함수의_시각화/practice.m
@@ -8,4 +8,11 @@ surf(X,Y,abs(log(z)),angle(log(z)));
 
 idx2plot = abs(Y)-mean(abs(Y))==0;
 hold on;
-plot3(linspace(1,3,50),zeros(1,50),abs(log(linspace(1,3,50))),'r')
+plot3(linspace(1,3,50),zeros(1,50),abs(log(linspace(1,3,50))),'r','linewidth',3)
+view(38.9, 16.4)
+
+xlabel('Real (z)');
+ylabel('Imag (z)');
+zlabel('|log(z)|');
+
+saveas(gcf,'complex_log.png')
