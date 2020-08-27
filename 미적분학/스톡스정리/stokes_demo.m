36a80eecea07236f2e9ec135ad1354c0b5b27f45
angeloyeo <angeloyeo@gmail.com>
2020-08-28 08:02:58 +0900

    스토크스 정리 블로그 그림 그리기 위한 시도

--- 미적분학/스톡스정리/stokes_demo.m
+++ 미적분학/스톡스정리/stokes_demo.m
@@ -9,14 +9,27 @@ x = R.*cos(T);
 y = R.*sin(T);
 
 z = surface_fun(x,y,-3);
-figure;
+figure('color','w');
 set(gcf,'position',[410 150 560 626]);
 
 surf(x,y,z,'facecolor',[0.5 0.8 0.5],'edgecolor','none');
+hold on;
+plot3(cos(theta), sin(theta), zeros(1, 500), 'r','linewidth',2)
+% arrow3([1, 0, 0], [1, 0.5, 0])
 caxis([-3 3])
 zlim([-3 3])
 grid on;
 camlight
+
+% gif 녹화를 위한 코드
+xlabel('$$x$$','interpreter','latex')
+ylabel('$$y$$','interpreter','latex')
+zlabel('$$z$$','interpreter','latex')
+axis vis3d
+for i = 1:360
+    camorbit(1, 0, 'data')
+    drawnow
+end
 
 %%
 nr = 1;
