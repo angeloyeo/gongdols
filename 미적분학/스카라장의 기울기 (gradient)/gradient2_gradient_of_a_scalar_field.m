6aa39f1804c1899e15f1a802e2aa92802d656118
angeloyeo <angeloyeo@gmail.com>
2020-09-14 17:47:31 +0900

    Update gradient2_gradient_of_a_scalar_field.m

--- 미적분학/스카라장의 기울기 (gradient)/gradient2_gradient_of_a_scalar_field.m
+++ 미적분학/스카라장의 기울기 (gradient)/gradient2_gradient_of_a_scalar_field.m
@@ -14,21 +14,21 @@ title('temperature or something')
 axis tight
 colorbar
 %% Gradient
-[fx,fy,fz]=gradient(T);
+[fx1,fy1,fz1]=gradient(T, 0.1); % spacing 인자 추가함. 2020년 9월 14일.
 
-% 위의 수치적 gradient와는 값이 다름.
-% 위의 수치적 gradient에서는 위치 간 간격이 미소길이보다는 크기 때문에 차이가 생기는 것으로 생각됨.
-% clear fx fy fz
+% 위의 수치적 gradient와는 값이 약간 차이를 보임.
+% 아마도 x, y, z의 범위에 비해 샘플 간 간격이 상대적으로 크다보니 생기는 에러는 아닐까 생각함.
+% clear fx2 fy2 fz2
 % for i=1:size(T,1)
 %     for j=1:size(T,2)
 %         for k=1:size(T,3)
-%             fx(i,j,k)=(-2)*lon(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
-%             fy(i,j,k)=(-2)*lat(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
-%             fz(i,j,k)=(-2)*z(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
+%             fx2(i,j,k)=20*(-2)*lon(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
+%             fy2(i,j,k)=20*(-2)*lat(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
+%             fz2(i,j,k)=20*(-2)*z(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
 %         end
 %     end
 % end
 
 
 hold on;
-quiver3(lon,lat,z,fx,fy,fz);
+quiver3(lon,lat,z,fx1,fy1,fz1);
