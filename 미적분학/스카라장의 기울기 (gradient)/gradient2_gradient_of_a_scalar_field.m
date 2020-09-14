dc79567c47b86cac1f52cb65d72c53f4dd51fba2
angeloyeo <angeloyeo@gmail.com>
2020-09-14 17:41:16 +0900

    Update gradient2_gradient_of_a_scalar_field.m
    gradient 값을 직접 계산하는 부분에 있는 오류 수정

--- 미적분학/스카라장의 기울기 (gradient)/gradient2_gradient_of_a_scalar_field.m
+++ 미적분학/스카라장의 기울기 (gradient)/gradient2_gradient_of_a_scalar_field.m
@@ -16,17 +16,19 @@ colorbar
 %% Gradient
 [fx,fy,fz]=gradient(T);
 
-% 
+% 위의 수치적 gradient와는 값이 다름.
+% 위의 수치적 gradient에서는 위치 간 간격이 미소길이보다는 크기 때문에 차이가 생기는 것으로 생각됨.
+% clear fx fy fz
 % for i=1:size(T,1)
 %     for j=1:size(T,2)
 %         for k=1:size(T,3)
-%             fx(i,j,k)=-2*lon(i)*exp(-lon(i)^2-lat(j)^2-z(k)^2);
-%             fy(i,j,k)=-2*lat(j)*exp(-lon(i)^2-lat(j)^2-z(k)^2);
-%             fz(i,j,k)=-2*z(k)*exp(-lon(i)^2-lat(j)^2-z(k)^2);
+%             fx(i,j,k)=(-2)*lon(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
+%             fy(i,j,k)=(-2)*lat(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
+%             fz(i,j,k)=(-2)*z(i,j,k)*exp(-lon(i,j,k)^2-lat(i,j,k)^2-z(i,j,k)^2);
 %         end
 %     end
 % end
-% 
+
 
 hold on;
-quiver3(lon,lat,z,fx,fy,fz)
+quiver3(lon,lat,z,fx,fy,fz);
