a1d8caaea63b411889c37857b46172125e5903b8
angeloyeo <angeloyeo@gmail.com>
2020-09-17 12:21:19 +0900

    rejection Sampling, MCMC 주요 코드 제외하고 모두 제거

--- 통계학/MCMC/metropolis_hastings/MATLAB_implementation2_additional_sampling/main.m
+++ 통계학/MCMC/metropolis_hastings/MATLAB_implementation2_additional_sampling/main.m
@@ -1,27 +1,44 @@
 clear; close all; clc;
+% 참고문헌: https://www.cs.ubc.ca/~arnaud/andrieu_defreitas_doucet_jordan_intromontecarlomachinelearning.pdf
+%
+%
 
-%% rejection sampling을 통해 data를 약간이나마 추출
-% target distribution과 유사 분포 
+rng(1)
+n_iter = 5000;
 target = @(x) 0.3*exp(-0.2 * x.^2) + 0.7 * exp(-0.2 * (x - 10).^2);
-pseudo_dist = @(x, mu, sigma) 1/(sigma*sqrt(2*pi)) * exp(-((x-mu).^2)/(2*sigma^2));
-
 xx = linspace(-10,20, 1000);
 
-figure; 
-plot(xx, target(xx))
-hold on;
-plot(xx, 20 * pseudo_dist(xx, 6.5, 4.5)) % 유사 분포는 target 분포를 감쌀 수 있어야 함.
+%%  Metropolis Hastings
 
-% rejection sampling 시작
-rng(1)
-n = 50000;
-x_q = randn(1, n) * 4.5 + 6.5; % 유사분포의 x 값을 sampling.
+% initialize x_0
+x = [];
+x = [x, (rand(1, 1) - 0.5) * 30 + 5]; % -10에서 20사이의 범위에서 random uniform
 
-crits = ...
-    target(x_q) ./ ...
-    (pseudo_dist(x_q, 6.5, 4.5) * 20); % 유사 분포에서 나온 값들을 envelope에 다시 집어넣기.
-coins = rand(1, length(crits));
+proposal = @(x, mu, s) 1/(s*sqrt(2*pi))*exp(-(x-mu)^2/(2*s^2)); % 정규분포
 
-x_p = x_q(coins<crits);
-figure; h = histogram(x_p,'BinWidth',0.5, 'Normalization','probability');
+my_std = 10;
+
+for i = 1 : n_iter
+    u = rand(1); % sample u
+    x_old = x(i);
+    
+    % 제안 분포 q(x_new | x_old) = N(x_old, sigma)로부터 샘플 추출
+    x_new = randn(1) * my_std + x_old;
+    
+    % A(x_old, x_new) 계산
+    A = min(1, ...
+        (target(x_new) * proposal(x_old, x_new, my_std)) / ...
+        (target(x_old) * proposal(x_new, x_old, my_std))...
+        );
+    
+    if u < A
+        x = [x x_new];
+        
+    else
+        x = [x x_old];
+    end
+end
+
+%% 결과 plot
+figure; h = histogram(x);
 hold on; plot(xx, target(xx)/max(target(xx))*max(h.Values))
--- 통계학/rejection_sampling/main.m
+++ 통계학/rejection_sampling/main.m
@@ -1,30 +1,11 @@
 clear; close all; clc;
 
 rng(1)
-n = 50000;
-
-%% rejection sampling을 통해 data를 sampling
-% target distribution
+n = 500;
 target = @(x) 0.3*exp(-0.2 * x.^2) + 0.7 * exp(-0.2 * (x - 10).^2);
-pseudo_dist = @(x, mu, sigma) 1/(sigma*sqrt(2*pi)) * exp(-((x-mu).^2)/(2*sigma^2));
-
 xx = linspace(-10,20, 1000);
 
-figure; 
-plot(xx, target(xx))
-hold on;
-plot(xx, 20 * pseudo_dist(xx, 6.5, 4.5))
-
-x_q = randn(1, n) * 4.5 + 6.5; 
-
-crits = target(x_q) ./ (pseudo_dist(x_q, 6.5, 4.5) * 20);
-coins = rand(1, length(crits));
-
-x_p_normal = x_q(coins<crits);
-figure; h = histogram(x_p_normal,'BinWidth',0.5, 'Normalization','probability');
-hold on; plot(xx, target(xx)/max(target(xx))*max(h.Values))
-
-%% 만약 rejection sampling에서 유사 분포를 uniform을 이용하면 어떨까?
+%% Uniform distribution을 proposal distribution으로 두고 sampling
 % 성능에 크게 차이는 없으나 reject되는 sample 수가 조금 차이가 있음.
 
 pseudo_dist2 = @(x) (x>=-10 * x<20) / 30;
@@ -34,7 +15,7 @@ plot(xx, target(xx));
 hold on;
 plot(xx, pseudo_dist2(xx)*21); % 21은 30 * 0.7을 얘기함. 30은 출력값이 1인 x의 범주, 0.7은 target의 최고 높이.
 
-x_q = (rand(1, n) - 0.5) * 30 + 5; % 10에서 20사이의 uniform distribution
+x_q = (rand(1, n) - 0.5) * 30 + 5; % -10에서 20사이의 uniform distribution
 
 crits = target(x_q) ./ (pseudo_dist2(x_q) * 21);
 coins = rand(1, length(crits));
