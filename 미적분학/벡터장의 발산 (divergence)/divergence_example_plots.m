8f425ab58464e8f716a8fe756ce0f2ff64942738
Donghoon Yeo <angeloyeo@gmail.com>
2020-10-19 18:44:14 +0900

    Update divergence_example_plots.m
    Streamline 나오도록 update

--- 미적분학/벡터장의 발산 (divergence)/divergence_example_plots.m
+++ 미적분학/벡터장의 발산 (divergence)/divergence_example_plots.m
@@ -21,10 +21,13 @@ imagesc(0:.125:2,0:.125:2,transpose(div))
 colormap(jet); colorbar
 alpha(0.2)
 
-% xy 평면에서 없어지고 생겨나는 벡터장
+%% xy 평면에서 없어지고 생겨나는 벡터장
 clear; close all; clc;
 
-[x,y]=meshgrid(-1:0.9:10,-1:0.9:10);
+% [x,y]=meshgrid(-1:0.9:10,-1:0.9:10);
+xx = linspace(-1, 10, 10);
+yy = xx;
+[x,y]=meshgrid(xx, yy);
 
 u=(x-2).*(x-8);
 v=(y-2).*(y-8);
@@ -34,12 +37,20 @@ grid on;
 xlabel('x');
 ylabel('y');
 
-
 div=divergence(x,y,u,v);
 
 hold on;
-imagesc(-1:0.9:10,-1:0.9:10,transpose(div))
-colormap(jet); y=colorbar;
-ylabel(y, 'divergence')
+imagesc(xx, yy ,transpose(div))
+colormap(jet); cbar=colorbar;
+ylabel(cbar, 'divergence')
 alpha(0.2)
 axis tight
+
+%% animation
+figure;
+[verts, averts] = streamslice(x, y, u, v);
+
+sl = streamline([verts, averts]);
+iverts = interpstreamspeed(x, y, u, v, verts, 0.01);
+streamparticles(iverts, 300, 'Animate', 15, 'FrameRate', 40, 'Markersize', 5);
+
